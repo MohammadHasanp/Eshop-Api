bde2ff6911f5137b78b4d3d37f64af240137c1ee
Mohammad Hasan Pirayandeh <PirayandehMohammadHasan@Gmail.com>
2025-08-13 22:14:04 +0330

    Complete-Product-Aggregate

---
+++ Common/Common.Domain/Utilitis/TextHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Common.Domain.Utilitis
+{
+    public static class TextHelper
+    {
+        //Slug Validation
+        public static string ToSlug(this string Text)
+        {
+            return Text.Trim().ToLower()
+                .Replace(" ", "-")
+                .Replace("+", "")
+                .Replace("_", "")
+                .Replace(")", "")
+                .Replace("(", "")
+                .Replace("*", "")
+                .Replace("&", "")
+                .Replace("^", "")
+                .Replace("%", "")
+                .Replace("$", "")
+                .Replace("#", "")
+                .Replace("@", "");
+        }
+        //ConvertHtmlToText
+        public static string ConvertHtmlToText(this string text)
+        {
+            return Regex.Replace(text, "<.*?>", " ")
+                .Replace(":&nbsp;", " ");
+        }
+    }
+}
---
+++ Common/Common.Domain/ValueObjects/SeoData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Domain.ValueObjects
+{
+    public class SeoData:ValueObject
+    {
+        public string MetaTitle { get;private set; }
+        public string MetaDescription { get; private set; }
+        public string MateKeyWords { get; private set; }
+        public string IndexPage { get; set; }
+        public string Canonical { get;private set; }
+        public string Schema { get; private set; }
+
+        public SeoData(string metaTitle, string metaDescription, string mateKeyWords, string indexPage, string canonical, string schema)
+        {
+            MetaTitle = metaTitle;
+            MetaDescription = metaDescription;
+            MateKeyWords = mateKeyWords;
+            IndexPage = indexPage;
+            Canonical = canonical;
+            Schema = schema;
+        }
+        public static SeoData CreateEmpty()
+        {
+            return new SeoData();
+        }
+        private SeoData() { }
+    }
+}
